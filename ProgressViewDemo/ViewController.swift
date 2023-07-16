a7da0f4f5ba5e776a1fe81dbaa25b90aece1497c
GG <rkrudtls@gmail.com>
2023-07-17 08:57:41 +0900

    Initial Commit

---
+++ ProgressViewDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProgressViewDemo
+//
+//  Created by Platfarm on 2023/07/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
